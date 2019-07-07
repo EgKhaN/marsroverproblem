bd6b5bad49a560b07dce44e249d060908164db87
Cemal Erdemir <cerdemir@gmail.com>
2019-07-07 16:44:28 +0300

    position tostring implemented

--- marsrover.console/Position.cs
+++ marsrover.console/Position.cs
@@ -5,5 +5,12 @@ namespace marsrover.console
         public int X { get; set; }
         public int Y { get; set; }
         public CardinalPoints Heading { get; set; }
+
+        public override string ToString()
+        {
+            var enumConverter = new CardinalPointEnumConverter();
+            var strHeading = enumConverter.ToString(Heading);
+            return $"{X} {Y} {strHeading}";
+        }
     }
 }
---
+++ marsrover.test/PositionTest.cs
@@ -0,0 +1,26 @@
+using marsrover.console;
+using Xunit;
+
+namespace marsrover.test
+{
+    public class PositionTest
+    {
+        [Theory]
+        [InlineData(13, 144, CardinalPoints.North, "13 144 N")]
+        [InlineData(45, 56, CardinalPoints.East, "45 56 E")]
+        [InlineData(3, 1, CardinalPoints.South, "3 1 S")]
+        [InlineData(7, 2, CardinalPoints.West, "7 2 W")]
+        public void CanToStringReturnProperString(int initalX, int initalY, CardinalPoints initalHeading, string expected)
+        {
+            var position = new Position
+            {
+                X = initalX,
+                Y = initalY,
+                Heading = initalHeading
+            };
+            var actual = position.ToString();
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
