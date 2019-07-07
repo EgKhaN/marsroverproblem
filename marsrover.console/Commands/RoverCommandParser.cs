00a243c68fbc5d4fb4a0218c553b6bce4a797127
Cemal Erdemir <cerdemir@gmail.com>
2019-07-07 21:08:29 +0300

    position parser removed

--- marsrover.console/Commands/RoverCommandParser.cs
+++ marsrover.console/Commands/RoverCommandParser.cs
@@ -6,14 +6,12 @@ namespace marsrover.console.Commands
     public class RoverCommandParser
     {
         public string Commands { get; set; }
-        public string RoverPosition { get; set; }
 
         public RoverCommandParser() { }
 
-        public RoverCommandParser(string roverPosition, string commands)
+        public RoverCommandParser(string commands)
         {
             Commands = commands;
-            RoverPosition = roverPosition;
         }
 
         public List<IRoverActionCommand> GetCommands()
@@ -37,25 +35,5 @@ namespace marsrover.console.Commands
             return commandList;
         }
 
-        public Position GetPosition()
-        {
-            var arrPosition = RoverPosition.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            var x = Convert.ToInt32(arrPosition[0]);
-            var y = Convert.ToInt32(arrPosition[1]);
-            var heading = ParseCardinalPoint(arrPosition[2]);
-            var position = new Position()
-            {
-                X = x,
-                Y = y,
-                Heading = heading
-            };
-            return position;
-        }
-
-        private CardinalPoints ParseCardinalPoint(string v)
-        {
-            var cardinalPointConverter = new CardinalPointEnumConverter();
-            return cardinalPointConverter.ToEnum(v);
-        }
     }
 }
