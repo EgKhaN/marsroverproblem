9baed13fff228503bf0f3d8dabe2ce67884c3024
Cemal Erdemir <cerdemir@gmail.com>
2019-07-07 16:47:04 +0300

    cardinal point tostring impemented

--- marsrover.console/CardinalPointEnumConverter.cs
+++ marsrover.console/CardinalPointEnumConverter.cs
@@ -7,10 +7,17 @@ namespace marsrover.console
         private Dictionary<string, CardinalPoints> _string2CardinalPointDict = new Dictionary<string, CardinalPoints>()
         { { "N", CardinalPoints.North }, { "E", CardinalPoints.East }, { "S", CardinalPoints.South }, { "W", CardinalPoints.West }
         };
+        private Dictionary<CardinalPoints, string> _cardinalPoint2StringDict = new Dictionary<CardinalPoints, string>()
+        { { CardinalPoints.North, "N" }, { CardinalPoints.East, "E" }, { CardinalPoints.South, "S" }, { CardinalPoints.West, "W" }
+        };
 
         public CardinalPoints ToEnum(string p)
         {
             return _string2CardinalPointDict[p];
+        }
+        public string ToString(CardinalPoints p)
+        {
+            return _cardinalPoint2StringDict[p];
         }
 
     }
