4be3497e782ed423c7c519807bc1b40e5a66ecd3
Cemal Erdemir <cerdemir@gmail.com>
2019-07-07 08:41:18 +0300

    inital commit

---
+++ marsrover.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace marsrover.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
