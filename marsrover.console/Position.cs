f6e9958ced58659258ba017f6fc5136a80ad76fe
Cemal Erdemir <cerdemir@gmail.com>
2019-07-07 09:20:27 +0300

    position classes renamed

---
+++ marsrover.console/Position.cs
@@ -0,0 +1,9 @@
+namespace marsrover.console
+{
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public CardinalPoints Heading { get; set; }
+    }
+}
